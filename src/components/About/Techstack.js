a2d5b110ab8260f5dfc1ac3085328b52563d1067
RehmanAkram <rehman@ideawake.com>
2024-04-15 08:52:17 +0500

    added text on hower

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -30,60 +30,85 @@ function Techstack() {
       
       <Col xs={4} md={2} className="tech-icons">
        <SiNestjs />
+       <span className="hover-text">Nest Js</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <span className="hover-text">Javascript</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript />
+        <span className="hover-text">Typescript</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <span className="hover-text">Node Js</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <span className="hover-text">React</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiExpress />
+        <span className="hover-text">Express</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
+        <span className="hover-text">Mongo Db</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAngular />
+        <span className="hover-text">Angular</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSequelize />
+        <span className="hover-text">Sequelize</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAwslambda />
+        <span className="hover-text">Lambda Functions</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiRedis />
+        <span className="hover-text">Redis</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
+        <span className="hover-text">Postgresql</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <span className="hover-text">Python</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAmazonaws />
+        <span className="hover-text">AWS</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGraphql />
+        <span className="hover-text">GraphQL</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiElasticsearch />
+        <span className="hover-text">Elastic Search</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiOpensearch />
+        <span className="hover-text">Open Search</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiOpenai />
+        <span className="hover-text">Open AI</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiDocker />
+        <span className="hover-text">Docker</span>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span className="small-text">TYPEORM</span>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <span className="small-text">Generative AI</span>
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -20,39 +20,51 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiMacos />
+        <span className="hover-text">MAC</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <span className="hover-text">VS Code</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <span className="hover-text">Postman</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
+        <span className="hover-text">Slack</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGithub />
+        <span className="hover-text">Github</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJira />
+        <span className="hover-text">JIRA</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiClickup />
+        <span className="hover-text">ClickUp</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiSkype />
+        <span className="hover-text">Skype</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGooglemeet />
+        <span className="hover-text">Google Meet</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiConfluence />
+        <span className="hover-text">Confluence</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiDocker />
+        <span className="hover-text">Docker</span>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiAmazonaws />
+        <span className="hover-text">AWS</span>
       </Col>
     </Row>
   );
